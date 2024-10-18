137a32c83d4e93c65f870f7fd982394d8f24caee
Ting Ting <fx24949@bristol.ac.uk>
2024-10-18 12:22:56 +0100

    Ting Ting Commit

---
+++ hello.c
@@ -0,0 +1,5 @@
+#include <stdio.h> 
+int main (void) {  //starts the definition of the main function
+	print("Hello World!\n"): //print line
+	return 0;      //sueccessful completion of program
+}                  //closes main function
